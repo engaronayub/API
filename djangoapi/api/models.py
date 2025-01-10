85be816755e53b0b4ac57ccf1ba9529641ada42e
Aron Ayub <aronayub248@gmail.com>
2025-01-10 20:50:17 +0300

    created serializer

--- djangoapi/api/models.py
+++ djangoapi/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Course(models.Model):
+    name = models.CharField(max_length = 250)
+    language =  models.CharField(max_length = 250)
+    price =  models.CharField(max_length = 10)
+    description = models.CharField(max_length=200)
+    instructor = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+
+
 # Create your models here.
